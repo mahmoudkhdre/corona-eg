69e3a69b5fa318ff9b2b2da96ac717d5c28bfb94
mahmoud khdre <mahmoudkhdre9@gmail.com>
2020-04-18 01:54:36 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component{
+//  const event = Date();
+  // Date= e.toISOString().substring(0, 10));
+ 
+  state={
+    Confirmed:[],
+    
+  }
+
+ 
+    
+  
+    componentDidMount(){
+      axios.get('https://api.covid19api.com/total/dayone/country/egypt').then(res =>{
+        console.log(res.data)
+        
+        this.setState({
+          Confirmed:res.data
+        })
+      })
+    }
+  render(){
+   
+    return(
+      <div className="App">
+        <div className="container">
+          <h3>عدد الاصابات في جمهورية  مصر العربية </h3>
+         {this.state.Confirmed.map(conf => <div className="total"> مجموع الاصابات <span className="num">{conf.Confirmed}</span>  <span className="his">  في يوم  {conf.Date.substring(0,10)} </span></div>)}
+        </div>
+        </div>
+    )
+  }
+}
+
+export default App;
